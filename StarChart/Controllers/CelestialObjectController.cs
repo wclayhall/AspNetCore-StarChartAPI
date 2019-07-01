de08199bc9e5030b005ddcb89f71c64b8ad94292
Clay Hall <wclayhall@comcast.net>
2019-07-01 19:51:54 -0400

    try

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +17,20 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+            if (item == null) return NotFound();
+
+            var satellites = _context.CelestialObjects.Where(s => s.OrbitedObjectId == item.Id).ToList();
+            if (satellites != null)
+            {
+                item.Satellites = satellites;
+            }
+            return Ok(item);
         }
     }
 }
